a488825b62f9d70610b8a4e34ae1041ee98ce367
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-18 12:37:52 -0300

    server project first commit

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes';
+
+const app = Fastify()
+
+app.register(cors)
+
+app.register(appRoutes)
+
+const PORT = 3333;
+
+app.listen({
+  port: PORT,
+}).then(() => console.log(`🗓 HTTP server running on port ${PORT}`))
