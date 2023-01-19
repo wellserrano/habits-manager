9697cec2eb6d1c9974f7568cad3ae46600b75fab
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-19 13:13:26 -0300

    BackButton functional

--- mobile/src/components/BackButton.tsx
+++ mobile/src/components/BackButton.tsx
@@ -1,10 +1,20 @@
 import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 import { Feather } from '@expo/vector-icons'
+
 import colors from 'tailwindcss/colors'
 
+
 export function BackButton() {
+  
+  const { goBack } = useNavigation();
+
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={ goBack }
+    >
       <Feather
         name="arrow-left"
         size={32}
