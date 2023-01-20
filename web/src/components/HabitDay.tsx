9bb2a1cb0b7ec7525fe72e04d05b1c40d05fa776
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-20 15:12:16 -0300

    fix: Popover shows date correctly

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import * as Popover from '@radix-ui/react-popover';
 import clsx from 'clsx';
+import dayjs from 'dayjs';
 import { Check } from 'phosphor-react';
 
 import { ProgressBar } from './ProgressBar'
@@ -11,8 +12,11 @@ interface HabitDayProps {
   amount?: number
 }
 
-export function HabitDay({ completed=0, amount=0 }: HabitDayProps) {
+export function HabitDay({ completed=0, amount=0, date }: HabitDayProps) {
   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0;
+
+  const dayAndMonth = dayjs(date).format('DD/MM')
+  const weekDay = dayjs(date).format('dddd')
 
   return(
     <Popover.Root>
@@ -29,8 +33,8 @@ export function HabitDay({ completed=0, amount=0 }: HabitDayProps) {
       />
       <Popover.Portal>
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
-          <span className='font-semibold text-zinc-400'>terça-feira</span>
-          <span className='mt-1 font-extrabold leading-tight text-3xl'>17/01</span>
+          <span className='font-semibold text-zinc-400'>{ weekDay }</span>
+          <span className='mt-1 font-extrabold leading-tight text-3xl'>{ dayAndMonth }</span>
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900'/>
           
