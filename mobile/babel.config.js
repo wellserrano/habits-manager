a88119e6da05ee0b1b927f808cf4aadfd7dc68b7
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-21 11:35:50 -0300

    fix: plugin reference

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'nativewind/babel',
-      'react-native-animated/plugin'
+      'react-native-reanimated/plugin'
     ]
   };
 };
